a23284da69c5c5a6b2bfe3b183863e2a909a8171
jakubkuzminski762-cmd <jakubkuzminski762@gmail.com>
2026-08-23 12:25:18 +0200

    Add Etap 1 Alpine interactions

--- game-platform/resources/js/app.js
+++ game-platform/resources/js/app.js
@@ -2,4 +2,41 @@ import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
+Alpine.data('uiShell', () => ({
+    mobileOpen: false,
+    modalOpen: false,
+    toastOpen: false,
+    lastFocused: null,
+
+    openMobile() {
+        this.mobileOpen = true;
+        this.$nextTick(() => this.$refs.mobileClose?.focus());
+    },
+
+    closeMobile() {
+        this.mobileOpen = false;
+        this.$nextTick(() => this.$refs.mobileTrigger?.focus());
+    },
+
+    openModal() {
+        this.lastFocused = document.activeElement;
+        this.modalOpen = true;
+        document.documentElement.style.overflow = 'hidden';
+        this.$nextTick(() => this.$refs.modalClose?.focus());
+    },
+
+    closeModal() {
+        this.modalOpen = false;
+        document.documentElement.style.overflow = '';
+        this.$nextTick(() => this.lastFocused?.focus());
+    },
+
+    showToast() {
+        this.toastOpen = true;
+        window.setTimeout(() => {
+            this.toastOpen = false;
+        }, 6000);
+    },
+}));
+
 Alpine.start();
